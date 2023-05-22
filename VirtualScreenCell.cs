d6cc242fdfc033786c11d638bdc7fe5c333f964f
Thomasgetacademy <thomasg@getacademy.no>
2023-05-22 08:15:50 +0200

    Add project files.

---
+++ VirtualScreenCell.cs
@@ -0,0 +1,76 @@
+﻿namespace RandomSquares;
+
+public class VirtualScreenCell
+{
+    public bool Up { get; private set; }
+    public bool Down { get; private set; }
+    public bool Left { get; private set; }
+    public bool Right { get; private set; }
+
+    public char GetCharacter()
+    {
+        var charToReturn =
+            Up && Right ? '└' :
+            Up && Left ? '┘' :
+            Down && Right ? '┌' :
+            Down && Left ? '┐' :
+            Left && !Right && !Up && !Down ? '─' :
+            Up && !Right && !Left && !Down ? '│' : ' ';
+        return charToReturn;
+
+        //if (Up && Down && Left && Right) return '┼';
+        //if (Up && Down && Left && !Right) return '┤';
+        //if (Up && Down && !Left && !Right) return '│';
+        //if (Up && Down && !Left && Right) return '├';
+
+        //if (Up && !Down && Left && !Right) return '┘';
+        //if (Up && !Down && Left && Right) return '┴';
+        //if (Up && !Down && !Left && !Right) return '╵';
+        //if (Up && !Down && !Left && Right) return '└';
+
+        //if (!Up && Down && Left && !Right) return '┐';
+        //if (!Up && Down && Left && Right) return '┬';
+        //if (!Up && Down && !Left && !Right) return '╷';
+        //if (!Up && Down && !Left && Right) return '┌';
+
+        //if (!Up && !Down && Left && !Right) return '╴';
+        //if (!Up && !Down && Left && Right) return '─';
+        //if (!Up && !Down && !Left && !Right) return ' ';
+        //if (!Up && !Down && !Left && Right) return '╶';
+        //return ' ';
+    }
+
+    public void AddHorizontal()
+    {
+        Left = true;
+    }
+
+    public void AddVertical()
+    {
+        Up = true;
+    }
+
+    public void AddLowerLeftCorner()
+    {
+        Up = true;
+        Right = true;
+    }
+
+    public void AddLowerRightCorner()
+    {
+        Up = true;
+        Left = true;
+    }
+
+    public void AddUpperLeftCorner()
+    {
+        Down = true;
+        Right = true;
+    }
+
+    public void AddUpperRightCorner()
+    {
+        Down = true;
+        Left = true;
+    }
+}
---
+++ VirtualScreenRow.cs
@@ -0,0 +1,53 @@
+﻿namespace RandomSquares;
+
+public class VirtualScreenRow
+{
+    private VirtualScreenCell[] _cells;
+
+    public VirtualScreenRow(int screenWidth)
+    {
+        _cells = new VirtualScreenCell[screenWidth];
+        for (int i = 0; i < _cells.Length; i++)
+        {
+            _cells[i] = new VirtualScreenCell();
+        }
+    }
+
+    public void AddBoxTopRow(int boxX, int boxWidth)
+    {
+        _cells[boxX].AddUpperLeftCorner();
+        var width = boxX + boxWidth - 1;
+        for (int i = boxX; i < width; i++)
+        {
+            _cells[i].AddHorizontal();
+        }
+        _cells[width].AddUpperRightCorner();
+    }
+
+    public void AddBoxBottomRow(int boxX, int boxWidth)
+    {
+        _cells[boxX].AddLowerLeftCorner();
+        var width = boxX + boxWidth - 1;
+        for (int i = boxX; i < width; i++)
+        {
+            _cells[i].AddHorizontal();
+        }
+        _cells[width].AddLowerRightCorner();
+    }
+
+    public void AddBoxMiddleRow(int boxX, int boxWidth)
+    {
+        var width = boxX + boxWidth - 1;
+        _cells[boxX].AddVertical();
+        _cells[width].AddVertical();
+    }
+
+    public void Show()
+    {
+        foreach (var cell in _cells)
+        {
+            Console.Write(cell.GetCharacter());
+        }
+        Console.WriteLine();
+    }
+}
